8d702246ac8c5b537dd9a7900957cc5c4d283df8
weicd770816 <863867674@qq.com>
2020-05-25 19:09:41 +0800

    添加文件1

--- src/main/java/com/distribute/lock/redissontest/util/RedisLockUtil.java
+++ src/main/java/com/distribute/lock/redissontest/util/RedisLockUtil.java
@@ -1,9 +1,12 @@
 package com.distribute.lock.redissontest.util;
 
+import com.distribute.lock.redissontest.component.DistributedLocker;
+
 /**
  * @author wcd
  * @create 2020/5/24-13:56
  */
 public class RedisLockUtil {
+    private static DistributedLocker distributedLocker=SpringContextHolder.getBean("distributedLocker",DistributedLocker.class);
 
 }
